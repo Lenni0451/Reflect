1f8c2077b39ec388cc529c8ea116ec42d2677275
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2022-08-18 00:46:03 +0200

    Added Classes#openModule

--- src/main/java/net/lenni0451/reflect/Classes.java
+++ src/main/java/net/lenni0451/reflect/Classes.java
@@ -48,6 +48,23 @@ public class Classes {
         Fields.copyObject(from, to, moduleField);
     }
 
+    /**
+     * Open a module of a class to everyone<br>
+     * This allows the usage of jdk internal classes which are normally protected by restricted module access
+     *
+     * @param clazz The class to open the module of
+     */
+    public static void openModule(final Class<?> clazz) {
+        Field moduleField = Fields.getDeclaredField(Class.class, "module");
+        if (moduleField == null) return;
+        Field everyoneModuleField = Fields.getDeclaredField(moduleField.getType(), "EVERYONE_MODULE");
+        Method implAddExportsOrOpens = Methods.getDeclaredMethod(moduleField.getType(), "implAddExportsOrOpens", String.class, moduleField.getType(), boolean.class, boolean.class);
+
+        Object everyone = Fields.get(null, everyoneModuleField);
+        Object module = Fields.get(clazz, moduleField);
+        Methods.invoke(module, implAddExportsOrOpens, Object.class.getPackage().getName(), everyone, true, true);
+    }
+
 
     /**
      * Get a class by its name<br>
