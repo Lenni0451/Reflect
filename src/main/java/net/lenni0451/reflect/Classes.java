fdc1498c6245019e19562535dd26a1a9882b19dc
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2022-08-26 18:49:57 +0200

    Moved module methods from Classes to Modules

--- src/main/java/net/lenni0451/reflect/Classes.java
+++ src/main/java/net/lenni0451/reflect/Classes.java
@@ -1,6 +1,5 @@
 package net.lenni0451.reflect;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import static net.lenni0451.reflect.JavaBypass.UNSAFE;
@@ -32,37 +31,6 @@ public class Classes {
     public static Class<?> getDeclaredClass(final Class<?> clazz, final String simpleName) {
         for (Class<?> c : getDeclaredClasses(clazz)) if (c.getSimpleName().equals(simpleName)) return c;
         return null;
-    }
-
-
-    /**
-     * Copy the module from one class to another<br>
-     * This bypasses all reflection restrictions in place
-     *
-     * @param from The class to copy the module from
-     * @param to   The class to copy the module to
-     */
-    public static void copyModule(final Class<?> from, final Class<?> to) {
-        Field moduleField = Fields.getDeclaredField(Class.class, "module");
-        if (moduleField == null) return;
-        Fields.copyObject(from, to, moduleField);
-    }
-
-    /**
-     * Open a module of a class to everyone<br>
-     * This allows the usage of jdk internal classes which are normally protected by restricted module access
-     *
-     * @param clazz The class to open the module of
-     */
-    public static void openModule(final Class<?> clazz) {
-        Field moduleField = Fields.getDeclaredField(Class.class, "module");
-        if (moduleField == null) return;
-        Field everyoneModuleField = Fields.getDeclaredField(moduleField.getType(), "EVERYONE_MODULE");
-        Method implAddExportsOrOpens = Methods.getDeclaredMethod(moduleField.getType(), "implAddExportsOrOpens", String.class, moduleField.getType(), boolean.class, boolean.class);
-
-        Object everyone = Fields.get(null, everyoneModuleField);
-        Object module = Fields.get(clazz, moduleField);
-        Methods.invoke(module, implAddExportsOrOpens, Object.class.getPackage().getName(), everyone, true, true);
     }
 
 
