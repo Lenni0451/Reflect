8c83b0888313ebd12c4e5f8bd62b438e8ebd5ed3
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-03-10 21:50:54 +0100

    Started moving code around for multi version implementations

--- src/main/java/net/lenni0451/reflect/accessor/FieldAccessor.java
+++ src/main/java/net/lenni0451/reflect/accessor/FieldAccessor.java
@@ -1,18 +1,15 @@
 package net.lenni0451.reflect.accessor;
 
-import net.lenni0451.reflect.Constructors;
-import net.lenni0451.reflect.Methods;
-import net.lenni0451.reflect.wrapper.ASMWrapper;
+import lombok.SneakyThrows;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.lang.reflect.Constructor;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.function.Supplier;
 
-import static net.lenni0451.reflect.wrapper.ASMWrapper.*;
+import static net.lenni0451.reflect.JavaBypass.TRUSTED_LOOKUP;
+import static net.lenni0451.reflect.utils.FieldInitializer.init;
+import static net.lenni0451.reflect.utils.FieldInitializer.reqInit;
 
 /**
  * Generate a getter/setter instance for a field.<br>
@@ -20,6 +17,23 @@ import static net.lenni0451.reflect.wrapper.ASMWrapper.*;
  * The implementation is generated using the internal ASM.
  */
 public class FieldAccessor {
+
+    private static final Class<?> FIELD_ACCESSOR_IMPL = reqInit(
+            () -> Class.forName("net.lenni0451.reflect.accessor.internal.ASMFieldAccessor"),
+            () -> new ClassNotFoundException("Unable to find FieldAccessor implementation class")
+    );
+    private static final MethodHandle MAKE_SETTER = init(
+            () -> TRUSTED_LOOKUP.findStatic(FIELD_ACCESSOR_IMPL, "makeSetter", MethodType.methodType(Object.class, Class.class, Object.class, Field.class))
+    );
+    private static final MethodHandle MAKE_DYNAMIC_SETTER = init(
+            () -> TRUSTED_LOOKUP.findStatic(FIELD_ACCESSOR_IMPL, "makeDynamicSetter", MethodType.methodType(Object.class, Class.class, Field.class))
+    );
+    private static final MethodHandle MAKE_GETTER = init(
+            () -> TRUSTED_LOOKUP.findStatic(FIELD_ACCESSOR_IMPL, "makeGetter", MethodType.methodType(Object.class, Class.class, Object.class, Field.class))
+    );
+    private static final MethodHandle MAKE_DYNAMIC_GETTER = init(
+            () -> TRUSTED_LOOKUP.findStatic(FIELD_ACCESSOR_IMPL, "makeDynamicGetter", MethodType.methodType(Object.class, Class.class, Field.class))
+    );
 
     /**
      * Create a new setter instance for the given field.<br>
@@ -33,39 +47,9 @@ public class FieldAccessor {
      * @param <I>          The invoker interface type
      * @return The setter instance
      */
+    @SneakyThrows
     public static <I> I makeSetter(@Nonnull final Class<?> invokerClass, final Object instance, @Nonnull final Field field) {
-        String newClassName = slash(field.getDeclaringClass()) + "$FieldSetter";
-        boolean staticField = Modifier.isStatic(field.getModifiers());
-        Method invokerMethod = findInvokerMethod(invokerClass, new Class[]{field.getType()}, void.class);
-        ASMWrapper acc = ASMWrapper.create(opcode("ACC_SUPER") | opcode("ACC_FINAL") | opcode("ACC_SYNTHETIC"), newClassName, null, "java/lang/Object", new String[]{slash(invokerClass)});
-
-        //noinspection Convert2MethodRef
-        addConstructor(acc, newClassName, () -> instance.getClass(), field);
-
-        ASMWrapper.MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), invokerMethod.getName(), desc(invokerMethod), null, null);
-        if (staticField) {
-            mv.visitVarInsn(getLoadOpcode(invokerMethod.getParameterTypes()[0]), 1);
-            if (!invokerMethod.getParameterTypes()[0].equals(field.getType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(field.getType()));
-            mv.visitFieldInsn(opcode("PUTSTATIC"), slash(field.getDeclaringClass()), field.getName(), desc(field.getType()));
-        } else {
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitFieldInsn(opcode("GETFIELD"), newClassName, "instance", desc(instance.getClass()));
-            mv.visitVarInsn(getLoadOpcode(invokerMethod.getParameterTypes()[0]), 1);
-            if (!invokerMethod.getParameterTypes()[0].equals(field.getType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(field.getType()));
-            mv.visitFieldInsn(opcode("PUTFIELD"), slash(field.getDeclaringClass()), field.getName(), desc(field.getType()));
-        }
-        mv.visitInsn(opcode("RETURN"));
-        mv.visitMaxs(2, 2);
-        mv.visitEnd();
-
-        Class<?> clazz = acc.defineMetafactory(field.getDeclaringClass());
-        if (staticField) {
-            Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz);
-            return (I) Constructors.invoke(constructor);
-        } else {
-            Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz, instance.getClass());
-            return (I) Constructors.invoke(constructor, instance);
-        }
+        return (I) MAKE_SETTER.invokeExact(invokerClass, instance, field);
     }
 
     /**
@@ -79,27 +63,9 @@ public class FieldAccessor {
      * @param <I>          The invoker interface type
      * @return The dynamic setter instance
      */
+    @SneakyThrows
     public static <I> I makeDynamicSetter(@Nonnull final Class<I> invokerClass, @Nonnull final Field field) {
-        if (Modifier.isStatic(field.getModifiers())) throw new IllegalArgumentException("Dynamic setter can only be used for non-static fields");
-        String newClassName = slash(field.getDeclaringClass()) + "$DynamicFieldSetter";
-        Method invokerMethod = findInvokerMethod(invokerClass, new Class[]{field.getDeclaringClass(), field.getType()}, void.class);
-        ASMWrapper acc = ASMWrapper.create(opcode("ACC_SUPER") | opcode("ACC_FINAL") | opcode("ACC_SYNTHETIC"), newClassName, null, "java/lang/Object", new String[]{slash(invokerClass)});
-
-        addConstructor(acc, newClassName, null, field);
-
-        ASMWrapper.MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), invokerMethod.getName(), desc(invokerMethod), null, null);
-        mv.visitVarInsn(opcode("ALOAD"), 1);
-        if (!invokerMethod.getParameterTypes()[0].equals(field.getDeclaringClass())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(field.getDeclaringClass()));
-        mv.visitVarInsn(getLoadOpcode(invokerMethod.getParameterTypes()[1]), 2);
-        if (!invokerMethod.getParameterTypes()[1].equals(field.getType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(field.getType()));
-        mv.visitFieldInsn(opcode("PUTFIELD"), slash(field.getDeclaringClass()), field.getName(), desc(field.getType()));
-        mv.visitInsn(opcode("RETURN"));
-        mv.visitMaxs(2, 3);
-        mv.visitEnd();
-
-        Class<?> clazz = acc.defineMetafactory(field.getDeclaringClass());
-        Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz);
-        return (I) Constructors.invoke(constructor);
+        return (I) MAKE_DYNAMIC_SETTER.invokeExact(invokerClass, field);
     }
 
     /**
@@ -114,36 +80,9 @@ public class FieldAccessor {
      * @param <I>          The invoker interface type
      * @return The getter instance
      */
+    @SneakyThrows
     public static <I> I makeGetter(@Nonnull final Class<I> invokerClass, final Object instance, @Nonnull final Field field) {
-        String newClassName = slash(field.getDeclaringClass()) + "$FieldGetter";
-        boolean staticField = Modifier.isStatic(field.getModifiers());
-        Method invokerMethod = findInvokerMethod(invokerClass, new Class[0], field.getType());
-        ASMWrapper acc = ASMWrapper.create(opcode("ACC_SUPER") | opcode("ACC_FINAL") | opcode("ACC_SYNTHETIC"), newClassName, null, "java/lang/Object", new String[]{slash(invokerClass)});
-
-        //noinspection Convert2MethodRef
-        addConstructor(acc, newClassName, () -> instance.getClass(), field);
-
-        ASMWrapper.MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), invokerMethod.getName(), desc(invokerMethod), null, null);
-        if (staticField) {
-            mv.visitFieldInsn(opcode("GETSTATIC"), slash(field.getDeclaringClass()), field.getName(), desc(field.getType()));
-        } else {
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitFieldInsn(opcode("GETFIELD"), newClassName, "instance", desc(instance.getClass()));
-            mv.visitFieldInsn(opcode("GETFIELD"), slash(field.getDeclaringClass()), field.getName(), desc(field.getType()));
-        }
-        if (!field.getType().equals(invokerMethod.getReturnType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(invokerMethod.getReturnType()));
-        mv.visitInsn(getReturnOpcode(invokerMethod.getReturnType()));
-        mv.visitMaxs(1, 1);
-        mv.visitEnd();
-
-        Class<?> clazz = acc.defineMetafactory(field.getDeclaringClass());
-        if (staticField) {
-            Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz);
-            return (I) Constructors.invoke(constructor);
-        } else {
-            Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz, instance.getClass());
-            return (I) Constructors.invoke(constructor, instance);
-        }
+        return (I) MAKE_GETTER.invokeExact(invokerClass, instance, field);
     }
 
     /**
@@ -157,72 +96,9 @@ public class FieldAccessor {
      * @param <I>          The invoker interface type
      * @return The dynamic getter instance
      */
+    @SneakyThrows
     public static <I> I makeDynamicGetter(@Nonnull final Class<I> invokerClass, @Nonnull final Field field) {
-        if (Modifier.isStatic(field.getModifiers())) throw new IllegalArgumentException("Dynamic setter can only be used for non-static fields");
-        String newClassName = slash(field.getDeclaringClass()) + "$DynamicFieldGetter";
-        Method invokerMethod = findInvokerMethod(invokerClass, new Class[]{field.getDeclaringClass()}, field.getType());
-        ASMWrapper acc = ASMWrapper.create(opcode("ACC_SUPER") | opcode("ACC_FINAL") | opcode("ACC_SYNTHETIC"), newClassName, null, "java/lang/Object", new String[]{slash(invokerClass)});
-
-        addConstructor(acc, newClassName, null, field);
-
-        ASMWrapper.MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), invokerMethod.getName(), desc(invokerMethod), null, null);
-        mv.visitVarInsn(opcode("ALOAD"), 1);
-        if (!invokerMethod.getParameterTypes()[0].equals(field.getDeclaringClass())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(field.getDeclaringClass()));
-        mv.visitFieldInsn(opcode("GETFIELD"), slash(field.getDeclaringClass()), field.getName(), desc(field.getType()));
-        if (!invokerMethod.getReturnType().equals(field.getType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(invokerMethod.getReturnType()));
-        mv.visitInsn(getReturnOpcode(invokerMethod.getReturnType()));
-        mv.visitMaxs(1, 2);
-        mv.visitEnd();
-
-        Class<?> clazz = acc.defineMetafactory(field.getDeclaringClass());
-        Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz);
-        return (I) Constructors.invoke(constructor);
-    }
-
-    private static Method findInvokerMethod(final Class<?> invokerClass, final Class<?>[] parameterTypes, final Class<?> returnType) {
-        if (!Modifier.isInterface(invokerClass.getModifiers())) throw new IllegalArgumentException("The invoker class must be an interface");
-
-        int abstractMethods = 0;
-        Method matched = null;
-        for (Method invokerMethod : Methods.getDeclaredMethods(invokerClass)) {
-            if (!Modifier.isAbstract(invokerMethod.getModifiers())) continue;
-            if (++abstractMethods > 1) throw new IllegalArgumentException("The invoker class must only have one abstract method");
-            if (invokerMethod.getParameterCount() != parameterTypes.length) throw new IllegalArgumentException("The invoker method must have " + parameterTypes.length + " parameters");
-            if (!invokerMethod.getReturnType().isAssignableFrom(returnType)) throw new IllegalArgumentException("The invoker method return type must be of type " + returnType.getName());
-
-            Class<?>[] invokerParameterTypes = invokerMethod.getParameterTypes();
-            for (int i = 0; i < invokerParameterTypes.length; i++) {
-                if (invokerParameterTypes[i].isAssignableFrom(parameterTypes[i])) continue;
-                throw new IllegalArgumentException("The invoker method parameter " + i + " must be of type " + parameterTypes[i].getName());
-            }
-            matched = invokerMethod;
-        }
-        if (matched == null) throw new IllegalArgumentException("Could not find a valid invoker method for: " + desc(parameterTypes, returnType));
-        return matched;
-    }
-
-    private static void addConstructor(final ASMWrapper acc, final String newClassName, @Nullable final Supplier<Class<?>> instanceType, final Field field) {
-        if (Modifier.isStatic(field.getModifiers()) || instanceType == null) {
-            ASMWrapper.MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), "<init>", "()V", null, null);
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitMethodInsn(opcode("INVOKESPECIAL"), "java/lang/Object", "<init>", "()V", false);
-            mv.visitInsn(opcode("RETURN"));
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
-        } else {
-            String instanceTypeDesc = desc(instanceType.get());
-            acc.visitField(opcode("ACC_PRIVATE") | opcode("ACC_FINAL"), "instance", instanceTypeDesc, null, null);
-
-            ASMWrapper.MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), "<init>", "(" + instanceTypeDesc + ")V", null, null);
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitMethodInsn(opcode("INVOKESPECIAL"), "java/lang/Object", "<init>", "()V", false);
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitVarInsn(opcode("ALOAD"), 1);
-            mv.visitFieldInsn(opcode("PUTFIELD"), newClassName, "instance", instanceTypeDesc);
-            mv.visitInsn(opcode("RETURN"));
-            mv.visitMaxs(2, 2);
-            mv.visitEnd();
-        }
+        return (I) MAKE_DYNAMIC_GETTER.invokeExact(invokerClass, field);
     }
 
 }
--- src/main/java/net/lenni0451/reflect/accessor/MethodAccessor.java
+++ src/main/java/net/lenni0451/reflect/accessor/MethodAccessor.java
@@ -1,15 +1,15 @@
 package net.lenni0451.reflect.accessor;
 
-import net.lenni0451.reflect.Constructors;
-import net.lenni0451.reflect.Methods;
-import net.lenni0451.reflect.wrapper.ASMWrapper;
+import lombok.SneakyThrows;
 
 import javax.annotation.Nonnull;
-import java.lang.reflect.Constructor;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
-import static net.lenni0451.reflect.wrapper.ASMWrapper.*;
+import static net.lenni0451.reflect.JavaBypass.TRUSTED_LOOKUP;
+import static net.lenni0451.reflect.utils.FieldInitializer.init;
+import static net.lenni0451.reflect.utils.FieldInitializer.reqInit;
 
 /**
  * Generate an invoker interface instance for a method.<br>
@@ -17,6 +17,17 @@ import static net.lenni0451.reflect.wrapper.ASMWrapper.*;
  * The implementation is generated at runtime using the internal ASM.
  */
 public class MethodAccessor {
+
+    private static final Class<?> FIELD_ACCESSOR_IMPL = reqInit(
+            () -> Class.forName("net.lenni0451.reflect.accessor.internal.ASMMethodAccessor"),
+            () -> new ClassNotFoundException("Unable to find MethodAccessor implementation class")
+    );
+    private static final MethodHandle MAKE_INVOKER = init(
+            () -> TRUSTED_LOOKUP.findStatic(FIELD_ACCESSOR_IMPL, "makeInvoker", MethodType.methodType(Object.class, Class.class, Object.class, Method.class))
+    );
+    private static final MethodHandle MAKE_DYNAMIC_INVOKER = init(
+            () -> TRUSTED_LOOKUP.findStatic(FIELD_ACCESSOR_IMPL, "makeDynamicInvoker", MethodType.methodType(Object.class, Class.class, Method.class))
+    );
 
     /**
      * Create a new invoker instance for the given method.<br>
@@ -30,64 +41,9 @@ public class MethodAccessor {
      * @param <I>          The invoker interface type
      * @return The invoker instance implementation
      */
+    @SneakyThrows
     public static <I> I makeInvoker(@Nonnull final Class<I> invokerClass, final Object instance, @Nonnull final Method method) {
-        String newClassName = slash(method.getDeclaringClass()) + "$MethodInvoker";
-        boolean staticMethod = Modifier.isStatic(method.getModifiers());
-        Method invokerMethod = findInvokerMethod(invokerClass, method, false);
-        ASMWrapper acc = ASMWrapper.create(opcode("ACC_SUPER") | opcode("ACC_FINAL") | opcode("ACC_SYNTHETIC"), newClassName, null, "java/lang/Object", new String[]{slash(invokerClass)});
-
-        if (staticMethod) {
-            MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), "<init>", "()V", null, null);
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitMethodInsn(opcode("INVOKESPECIAL"), "java/lang/Object", "<init>", "()V", false);
-            mv.visitInsn(opcode("RETURN"));
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
-        } else {
-            String instanceType = desc(instance.getClass());
-            acc.visitField(opcode("ACC_PRIVATE"), "instance", instanceType, null, null);
-
-            MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), "<init>", "(" + instanceType + ")V", null, null);
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitMethodInsn(opcode("INVOKESPECIAL"), "java/lang/Object", "<init>", "()V", false);
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitVarInsn(opcode("ALOAD"), 1);
-            mv.visitFieldInsn(opcode("PUTFIELD"), newClassName, "instance", instanceType);
-            mv.visitInsn(opcode("RETURN"));
-            mv.visitMaxs(2, 2);
-            mv.visitEnd();
-        }
-
-        String methodClass = slash(method.getDeclaringClass());
-        String methodDesc = desc(method);
-        boolean interfaceMethod = Modifier.isInterface(method.getDeclaringClass().getModifiers());
-        MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), invokerMethod.getName(), desc(invokerMethod), null, null);
-        if (staticMethod) {
-            pushArgs(mv, invokerMethod.getParameterTypes(), method.getParameterTypes());
-            mv.visitMethodInsn(opcode("INVOKESTATIC"), methodClass, method.getName(), methodDesc, interfaceMethod);
-        } else {
-            mv.visitVarInsn(opcode("ALOAD"), 0);
-            mv.visitFieldInsn(opcode("GETFIELD"), newClassName, "instance", desc(instance.getClass()));
-            pushArgs(mv, invokerMethod.getParameterTypes(), method.getParameterTypes());
-            if (interfaceMethod) {
-                mv.visitMethodInsn(opcode("INVOKEINTERFACE"), methodClass, method.getName(), methodDesc, true);
-            } else {
-                mv.visitMethodInsn(opcode("INVOKEVIRTUAL"), methodClass, method.getName(), methodDesc, false);
-            }
-        }
-        if (!method.getReturnType().equals(invokerMethod.getReturnType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(invokerMethod.getReturnType()));
-        mv.visitInsn(getReturnOpcode(invokerMethod.getReturnType()));
-        mv.visitMaxs(invokerMethod.getParameterCount() + 1, invokerMethod.getParameterCount() + 1);
-        mv.visitEnd();
-
-        Class<?> clazz = acc.defineMetafactory(method.getDeclaringClass());
-        if (Modifier.isStatic(method.getModifiers())) {
-            Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz);
-            return (I) Constructors.invoke(constructor);
-        } else {
-            Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz, instance.getClass());
-            return (I) Constructors.invoke(constructor, instance);
-        }
+        return (I) MAKE_INVOKER.invokeExact(invokerClass, instance, method);
     }
 
     /**
@@ -101,86 +57,9 @@ public class MethodAccessor {
      * @param <I>          The invoker interface type
      * @return The invoker instance implementation
      */
+    @SneakyThrows
     public static <I> I makeDynamicInvoker(@Nonnull final Class<I> invokerClass, @Nonnull final Method method) {
-        if (Modifier.isStatic(method.getModifiers())) throw new IllegalArgumentException("Dynamic invoker can only be used for non-static methods");
-        String newClassName = slash(method.getDeclaringClass()) + "$DynamicMethodInvoker";
-        Method invokerMethod = findInvokerMethod(invokerClass, method, true);
-        ASMWrapper acc = ASMWrapper.create(opcode("ACC_SUPER") | opcode("ACC_FINAL") | opcode("ACC_SYNTHETIC"), newClassName, null, "java/lang/Object", new String[]{slash(invokerClass)});
-
-        MethodVisitorAccess mv = acc.visitMethod(opcode("ACC_PUBLIC"), "<init>", "()V", null, null);
-        mv.visitVarInsn(opcode("ALOAD"), 0);
-        mv.visitMethodInsn(opcode("INVOKESPECIAL"), "java/lang/Object", "<init>", "()V", false);
-        mv.visitInsn(opcode("RETURN"));
-        mv.visitMaxs(1, 1);
-        mv.visitEnd();
-
-        mv = acc.visitMethod(opcode("ACC_PUBLIC"), invokerMethod.getName(), desc(invokerMethod), null, null);
-        pushArgs(mv, invokerMethod.getParameterTypes(), prepend(method.getParameterTypes(), method.getDeclaringClass()));
-        if (Modifier.isInterface(method.getDeclaringClass().getModifiers())) {
-            mv.visitMethodInsn(opcode("INVOKEINTERFACE"), slash(method.getDeclaringClass()), method.getName(), desc(method), true);
-        } else {
-            mv.visitMethodInsn(opcode("INVOKEVIRTUAL"), slash(method.getDeclaringClass()), method.getName(), desc(method), false);
-        }
-        if (!method.getReturnType().equals(invokerMethod.getReturnType())) mv.visitTypeInsn(opcode("CHECKCAST"), slash(invokerMethod.getReturnType()));
-        mv.visitInsn(getReturnOpcode(invokerMethod.getReturnType()));
-        mv.visitMaxs(invokerMethod.getParameterCount() + 1, invokerMethod.getParameterCount() + 1);
-        mv.visitEnd();
-
-        Class<?> clazz = acc.defineMetafactory(method.getDeclaringClass());
-        Constructor<?> constructor = Constructors.getDeclaredConstructor(clazz);
-        return (I) Constructors.invoke(constructor);
-    }
-
-    private static Method findInvokerMethod(final Class<?> invokerClass, final Method method, final boolean requireInstance) {
-        if (!Modifier.isInterface(invokerClass.getModifiers())) throw new IllegalArgumentException("The invoker class must be an interface");
-
-        int abstractMethods = 0;
-        Method matched = null;
-        for (Method invokerMethod : Methods.getDeclaredMethods(invokerClass)) {
-            if (!Modifier.isAbstract(invokerMethod.getModifiers())) continue;
-            if (++abstractMethods > 1) throw new IllegalArgumentException("The invoker class must only have one abstract method");
-            if (invokerMethod.getParameterCount() != method.getParameterCount() + (requireInstance ? 1 : 0)) {
-                throw new IllegalArgumentException("The invoker method must have " + (method.getParameterCount() + (requireInstance ? 1 : 0)) + " parameters");
-            }
-            if (!invokerMethod.getReturnType().isAssignableFrom(method.getReturnType())) {
-                throw new IllegalArgumentException("The invoker method return type must be of type " + method.getReturnType().getName());
-            }
-
-            Class<?>[] invokerParameterTypes = invokerMethod.getParameterTypes();
-            Class<?>[] methodParameterTypes = method.getParameterTypes();
-            for (int i = 0; i < invokerParameterTypes.length; i++) {
-                Class<?> invokerParameterType = invokerParameterTypes[i];
-                Class<?> methodParameterType = (requireInstance && i == 0) ? method.getDeclaringClass() : methodParameterTypes[i - (requireInstance ? 1 : 0)];
-                if (invokerParameterType.isAssignableFrom(methodParameterType)) continue;
-                throw new IllegalArgumentException("The invoker method parameter " + i + " must be of type " + methodParameterType);
-            }
-            matched = invokerMethod;
-        }
-        if (matched == null) throw new IllegalArgumentException("Could not find a valid invoker method for: " + method);
-        return matched;
-    }
-
-    private static void pushArgs(final MethodVisitorAccess mv, final Class<?>[] supplied, final Class<?>[] target) {
-        int stack = 1;
-        for (int i = 0; i < supplied.length; i++) {
-            Class<?> suppliedType = supplied[i];
-            Class<?> targetType = target[i];
-            mv.visitVarInsn(getLoadOpcode(suppliedType), stack);
-            if (!suppliedType.equals(targetType)) mv.visitTypeInsn(opcode("CHECKCAST"), slash(targetType));
-            stack += getStackSize(suppliedType);
-        }
-    }
-
-    private static int getStackSize(final Class<?> clazz) {
-        if (long.class.equals(clazz) || double.class.equals(clazz)) return 2;
-        return 1;
-    }
-
-    private static Class<?>[] prepend(final Class<?>[] classes, final Class<?> other) {
-        Class<?>[] newClasses = new Class<?>[classes.length + 1];
-        newClasses[0] = other;
-        System.arraycopy(classes, 0, newClasses, 1, classes.length);
-        return newClasses;
+        return (I) MAKE_DYNAMIC_INVOKER.invokeExact(invokerClass, method);
     }
 
 }
