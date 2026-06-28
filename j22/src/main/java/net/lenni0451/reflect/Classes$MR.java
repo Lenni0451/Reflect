55bd927aa8719f9b7175f2adb23797db60a8484a
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-06-28 15:43:35 +0200

    Use DROP_METHOD_INFO in Java 22+ for Classes#getCallerClass

--- j22/src/main/java/net/lenni0451/reflect/Classes$MR.java
+++ j22/src/main/java/net/lenni0451/reflect/Classes$MR.java
@@ -2,9 +2,11 @@ package net.lenni0451.reflect;
 
 import lombok.SneakyThrows;
 
+import java.util.Set;
+
 class Classes$MR {
 
-    private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+    private static final StackWalker STACK_WALKER = StackWalker.getInstance(Set.of(StackWalker.Option.RETAIN_CLASS_REFERENCE, StackWalker.Option.DROP_METHOD_INFO));
 
     @SneakyThrows
     public static Class<?> getCallerClass(final int depth) {
